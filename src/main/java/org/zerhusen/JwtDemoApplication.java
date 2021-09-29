2a057b1a19147710919dffa2521e2280c798fc1e
nameabc <a@test.com>
2021-09-29 12:24:38 -0700

    set Java 1.8

--- src/main/java/org/zerhusen/JwtDemoApplication.java
+++ src/main/java/org/zerhusen/JwtDemoApplication.java
@@ -8,8 +8,9 @@ public class JwtDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JwtDemoApplication.class, args);
+        System.out.println("java version " + System.getProperty("java.version"));
         System.out.println("\nMock Primary IAM started. ");
-       System.out.println("This is a Java backend app that performance user authentication and mocks the customer's primary IAM. ");
-         System.out.println("It authenticates the username against a local db and returns an access token.");
+        System.out.println("This is a Java backend app that performance user authentication and mocks the customer's primary IAM. ");
+        System.out.println("It authenticates the username against a local db and returns an access token.");
     }
 }
